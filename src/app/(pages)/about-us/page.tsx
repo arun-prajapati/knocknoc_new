33dc7d36b2f30d1127cbb026608b91c82ed17888
manish <panchalmanihs163@gmail.com>
2024-09-26 17:58:03 +0530

    about us page

--- src/app/(pages)/about-us/page.tsx
+++ src/app/(pages)/about-us/page.tsx
@@ -1,11 +1,12 @@
 import About from "@/pagesContain/About";
+import SecondAboutPage from "@/pagesContain/About/SecondAboutPage";
 import React from "react";
 
 const page = () => {
   return (
     <main className="profile_page profile_layout 2xl:container px-2  ">
       <div className="w-full mx-auto max-w-4xl pt-10">
-        <About />
+        <SecondAboutPage />
       </div>
     </main>
   );
